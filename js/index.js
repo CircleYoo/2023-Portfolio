812bee5bdbf2b827976d29261d170af90d35e989
유정원 <87813501+CircleYoo@users.noreply.github.com>
2023-09-04 18:18:09 +0900

    making header

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// header button
+let $hamBtn = document.querySelector(".hamburger_box");
+$hamBtn.addEventListener('click', function () {
+  this.classList.toggle('active');
+})
+console.log($hamBtn)
