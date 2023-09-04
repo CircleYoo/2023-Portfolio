d98fe0f9e438cf24d7c65b98fc80ce2fb04643d7
유정원 <87813501+CircleYoo@users.noreply.github.com>
2023-09-04 22:35:25 +0900

    making visual

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
 // header button
-let $hamBtn = document.querySelector(".hamburger_box");
+const $hamBtn = document.querySelector(".hamburger_box");
+const $menu = document.querySelector(".fullscreen_menu");
+const $linkArea = document.querySelectorAll(".fullscreen_menu > .link_area a");
+
 $hamBtn.addEventListener('click', function () {
   this.classList.toggle('active');
+  $menu.classList.toggle('on');
 })
-console.log($hamBtn)
+
+// $linkArea.forEach(element => {
+//   element.addEventListener('click', function () {
+//     $menu.classList.remove("on");
+//   })
+// });
+console.log($linkArea);
+
+const $rotateIcon = document.querySelector(".rounded_shape > img");
+function scrollRotate() {
+  $rotateIcon.style.transform = "rotate(" + window.scrollY / 2 + "deg)";
+}
+window.addEventListener("scroll", function () {
+  let pos = window.scrollY;
+  scrollRotate();
+})
+
