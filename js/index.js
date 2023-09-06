5ae1b42908916cb6e3d2a630610bd6acb1c826e1
유정원 <87813501+CircleYoo@users.noreply.github.com>
2023-09-06 16:33:59 +0900

    complete making mb_filter

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ const $hamBtn = document.querySelector(".hamburger_box");
 const $menu = document.querySelector(".fullscreen_menu");
 const $linkArea = document.querySelectorAll(".fullscreen_menu > .link_area a");
 
-$hamBtn.addEventListener('click', function () {
-  this.classList.toggle('active');
-  $menu.classList.toggle('on');
-})
+$hamBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  $menu.classList.toggle("on");
+});
 
 // $linkArea.forEach(element => {
 //   element.addEventListener('click', function () {
@@ -22,16 +22,39 @@ function scrollRotate() {
 window.addEventListener("scroll", function () {
   let pos = window.scrollY;
   scrollRotate();
-})
+});
 
 // 모바일 필터 버튼 보이기
-const $optionBtn = document.querySelector('.label');
-const $optionBtnSpan = document.querySelector(".more_btn span");
-const $option = document.querySelector('.option_list');
+const $label = document.querySelector(".label");
+const $labelTitle = $label.querySelector("h3");
+const $moreBtn = document.querySelector(".more_btn span");
+const $option = document.querySelector(".option_list");
+const $options = document.querySelectorAll(".option_item");
 
-$optionBtn.addEventListener('click', function () {
-  console.log('ddd');
-  $option.classList.toggle('on');
-  $optionBtnSpan.classList.toggle("on");
-})
+// 클릭한 옵션의 텍스트를 .label 안에 넣음
+const handleSelect = (item) => {
+  $labelTitle.innerHTML = item.textContent;
+};
 
+
+// 옵션 몰록이 열림/닫힘
+const toggleOptions = () => {
+  if ($option.classList.contains("on")) {
+    $option.classList.remove("on");
+    $moreBtn.classList.remove("on");
+  } else {
+    $option.classList.add("on");
+    $moreBtn.classList.add("on");
+  }
+};
+
+// 옵션 클릭시, 클릭한 옵션을 넘기고 열림/닫힘
+$options.forEach((option) => {
+  option.addEventListener("click", () => {
+    handleSelect(option);
+    toggleOptions();
+  });
+});
+
+// 라벨 클릭시, 옵션 몰록이 열림/닫힘
+$label.addEventListener("click", toggleOptions);
