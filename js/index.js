097c28eb11d65276a102be07cd8fa5b4734ddfaa
유정원 <87813501+CircleYoo@users.noreply.github.com>
2023-09-06 20:56:53 +0900

    complete making projects

--- js/index.js
+++ js/index.js
@@ -1,19 +1,14 @@
-// header button
-const $hamBtn = document.querySelector(".hamburger_box");
+// (시작) 햄버거 버튼 열림/닫힘
+const $hamBtn = document.querySelector(".hamburger");
+const $hamSpan = document.querySelector(".hamburger_box");
 const $menu = document.querySelector(".fullscreen_menu");
 const $linkArea = document.querySelectorAll(".fullscreen_menu > .link_area a");
 
 $hamBtn.addEventListener("click", function () {
-  this.classList.toggle("active");
+  $hamSpan.classList.toggle("active");
   $menu.classList.toggle("on");
 });
-
-// $linkArea.forEach(element => {
-//   element.addEventListener('click', function () {
-//     $menu.classList.remove("on");
-//   })
-// });
-console.log($linkArea);
+// (끝) 햄버거 버튼 열림/닫힘
 
 const $rotateIcon = document.querySelector(".rounded_shape > img");
 function scrollRotate() {
@@ -24,7 +19,7 @@ window.addEventListener("scroll", function () {
   scrollRotate();
 });
 
-// 모바일 필터 버튼 보이기
+// (시작) 모바일 필터 버튼 보이기
 const $label = document.querySelector(".label");
 const $labelTitle = $label.querySelector("h3");
 const $moreBtn = document.querySelector(".more_btn span");
@@ -35,7 +30,6 @@ const $options = document.querySelectorAll(".option_item");
 const handleSelect = (item) => {
   $labelTitle.innerHTML = item.textContent;
 };
-
 
 // 옵션 몰록이 열림/닫힘
 const toggleOptions = () => {
@@ -58,3 +52,77 @@ $options.forEach((option) => {
 
 // 라벨 클릭시, 옵션 몰록이 열림/닫힘
 $label.addEventListener("click", toggleOptions);
+// (끝) 모바일 필터 버튼 보이기
+
+// (시작) 프로젝트 리스트 가져오기
+const $projectWrapper = document.querySelector(".project_area");
+
+
+let projectList = null;
+function getData() {
+  fetch("data/projectdata.json")
+    .then((res) => res.json())
+    .then((result) => {
+      projectList = result;
+      makeList(result.projects);
+    })
+    .catch((err) => console.log("project 가져오기 실패", err));
+}
+
+function makeList(items) {
+  $projectWrapper.innerHTML = null;
+  items.forEach((item) => {
+    const result = makeItem(item);
+    $projectWrapper.appendChild(result);
+  });
+}
+
+function makeItem(item) {
+  const div = document.createElement("div");
+  div.classList.add("project_item");
+
+  // 키워드 각각 <span> 태그로 가져오기
+  const keyword = item.keyword
+  .map((keyword) => `<span class="keyword">${keyword}</span>`)
+  .join("");
+  
+  // review, github, origin 링크 조건부 출력
+  let review = item.review ? `<a href="${item.review}" target="_blank">Review</a>` : '';
+  let github = item.github
+    ? `<a href="${item.github}" target="_blank">Gihub</a>`
+    : "";
+  let origin = item.original
+    ? `<a href="${item.original}" target="_blank">Original</a>`
+    : "";
+
+  div.innerHTML = `
+    <div class="img_wrap">
+      <img class="item_img" src="${item.img}" alt="${item.alt}">
+      <div class="img_hover">
+        <div class="more_link">
+          ${review}
+          ${github}
+          ${origin}
+        </div>
+        <a href="${item.create}" class="link" target="_blank">
+          <img src="./img/arrow_small.svg" alt="바로가기">
+          <span>view</span>
+        </a>
+      </div>
+    </div>
+    <h3 class="projects_title">${item.title}</h3>
+    <span>${item.text}</span>
+    <div class="projects_desc">
+      <span class="contribution">기여도 ${item.contribution}%</span>
+      <span class="keyword_area">${keyword}</span>
+    </div>
+  `;
+  //
+
+
+  return div;
+}
+
+
+getData();
+// (끝) 프로젝트 리스트 가져오기
